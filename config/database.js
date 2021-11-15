3f8dc0a294d392d8f06e3109866a9f53c8226def
joselopez59 <50359100+joselopez59@users.noreply.github.com>
2021-11-15 19:05:41 +0100

    database.js + git.ignore

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('DATABASE_CONNECTION_NAME'),
   connections: {
-    default: {
+    sqlite: {
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
@@ -11,5 +11,18 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+    mysql: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    }
   },
 });
