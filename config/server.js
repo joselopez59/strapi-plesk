0850fa1008bed223150bae5dd1ea7f5d6e93e965
joselopez59 <50359100+joselopez59@users.noreply.github.com>
2021-11-15 18:16:24 +0100

    initial strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fd1532a01dc8ba86a29a84d27f40113f'),
+    },
+  },
+});
